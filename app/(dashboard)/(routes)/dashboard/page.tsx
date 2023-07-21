90219928282bc93f8258f95b0c9e0f725babc8f8
Chence Shi <ascc594557067@gmail.com>
2023-07-20 19:59:54 -0700

    feat: add sidebar

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,8 +1,8 @@
-import { Button } from '@/components/ui/button';
-
 const DashboardPage = () => {
   return (
-    <p>Dashboard Page (protected)</p>
+    <div>
+      <p>Dashboard Page (protected)</p>
+    </div>
   );
 };
 
